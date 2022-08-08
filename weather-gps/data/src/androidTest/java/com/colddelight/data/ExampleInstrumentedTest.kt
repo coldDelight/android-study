b68eed342f22a6da0aa031add353e37f81cb2908
coldDelight <hno05039@naver.com>
2022-08-08 16:45:48 +0900

    weather-gps :다시 init

--- weather-gps/data/src/androidTest/java/com/colddelight/data/ExampleInstrumentedTest.kt
+++ weather-gps/data/src/androidTest/java/com/colddelight/data/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.colddelight.data", appContext.packageName)
+        assertEquals("com.colddelight.data.test", appContext.packageName)
     }
 }
